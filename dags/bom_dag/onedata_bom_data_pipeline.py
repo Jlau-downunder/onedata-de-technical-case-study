b2b40e91890de84436ec8866483d2de02636cbb9
Jeremy Lau <aus.jeremy.lau@gmail.com>
2023-11-10 00:12:27 +1100

    dag definition clean up

--- dags/bom_dag/onedata_bom_data_pipeline.py
+++ dags/bom_dag/onedata_bom_data_pipeline.py
@@ -6,6 +6,7 @@ import os
 
 #os.chdir('opt/airflow')
 
+dag_dir = '/opt/airflow/dags/bom_dag/py_callables/'
 
 # Define your DAG
 with DAG(
@@ -17,14 +18,14 @@ with DAG(
         'owner': 'airflow',
     }
 ) as dag:
-    
+
     task_0 = DummyOperator(
         task_id='Pipeline_start',
         dag=dag,
     )
 
 
-    task1_script_path = '/opt/airflow/dags/bom_dag/py_callables/task_1.py'
+    task1_script_path = f"{dag_dir}task_1.py"
     task_1 = BashOperator(
         task_id='bom_csv_data_prep',
         bash_command=f'python {task1_script_path}',
@@ -32,7 +33,7 @@ with DAG(
     )
 
 
-    task2_script_path = '/opt/airflow/dags/bom_dag/py_callables/task_2.py'
+    task2_script_path = f"{dag_dir}task_2.py"
     task_2 = BashOperator(
         task_id='load_data_into_file_stage',
         bash_command=f'python {task2_script_path}',
@@ -40,21 +41,21 @@ with DAG(
     )
 
 
-    task3_script_path = '/opt/airflow/dags/bom_dag/py_callables/task_3.py'
+    task3_script_path = f"{dag_dir}task_3.py"
     task_3 = BashOperator(
         task_id='load_stage_files_into_clean_layer',
         bash_command=f'python {task3_script_path}',
         dag=dag,
     )
 
-    task4_script_path = '/opt/airflow/dags/bom_dag/py_callables/task_4.py'
+    task4_script_path = f"{dag_dir}task_4.py"
     task_4 = BashOperator(
         task_id='model_and_partition_data',
         bash_command=f'python {task4_script_path}',
         dag=dag,
     )
 
-    task5_script_path = '/opt/airflow/dags/bom_dag/py_callables/task_5.py'
+    task5_script_path = f"{dag_dir}task_5.py"
     task_5 = BashOperator(
         task_id='table_reconciliation_checks',
         bash_command=f'python {task5_script_path}',
@@ -67,7 +68,7 @@ with DAG(
     )
 
 
-    # Define the order in which tasks should run
+
     task_0 >> task_1 >> task_2 >> task_3 >> task_4 >> task_5 >> task_6
 
     if __name__ == "__main__":
